f0b68b5c769439f22b9ffd94648947f5548911cc
Peter Wildfeuer <wildfeuer.peter@gmail.com>
2015-07-26 17:53:26 -0400

    final first draft to be sent to engineers

--- 07_25_15_IrisFINAL.py
+++ 07_25_15_IrisFINAL.py
@@ -4,33 +4,42 @@ import math as m
 
 # Get the surface object
 srf = rs.GetObject("Select surface", rs.filter.surface, True)
-if not srf: return
 
 # Get the number of rows
 rows = rs.GetInteger("Number of rows", 2, 2)
-if not rows: return
 
 # Get the number of columns
 cols = rs.GetInteger("Number of columns", 2, 2)
-if not cols: return
 
 ref=rs.GetObject("please select reference curve",rs.filter.curve)
 joint= rs.GetObject("please select joint geometry",rs.filter.polysurface)
-arm=rs.GetObject("please select arm geometry"),rs.filter.polysurface)
-link=rs.GetObject("please select link geometry")
-gap=rs.GetReal("please indicate the spacing between arms in section")
-holeRad=rs.GetReal("please indicate the hole width for link input")
+arm=rs.GetObject("please select arm geometry",rs.filter.polysurface)
+smallLink=rs.GetObject("please select 4-link geometry")
+bigLink=rs.GetObject("please select 2-link geometry")
+gap=rs.GetReal("please indicate the spacing between arms in section",.0625)
+holeRad=rs.GetReal("please indicate the hole width for link input",.125)
 
 
-def attachLinks(link,crvs,pts,ref):
-    links=[]
+def attachLinks(bigLink,smallLink,crvs,smallPts,ref,cols):
+    bigLinks=[]
+    smallLinks=[]
+    bigPts=[]
     for i in range(len(crvs)):
-        pts.append(rs.CurveEndPoint(crvs[i]))
-        pts.append(rs.CurveStartPoint(crvs[i]))
-    pts=removeDup(pts)
-    for i in range(len(pts)):
-        links.append(rs.CopyObject(link,rs.VectorCreate(pts[i],rs.CurveStartPoint(ref))))
-    return links
+        if i%(cols*2-2)!=0:
+            bigPts.append(rs.CurveEndPoint(crvs[i]))
+            bigPts.append(rs.CurveStartPoint(crvs[i]))
+        else:
+            smallPts.append(rs.CurveStartPoint(crvs[i]))
+            smallPts.append(rs.CurveStartPoint(crvs[i+1]))
+            smallPts.append(rs.CurveEndPoint(crvs[i-1]))
+            smallPts.append(rs.CurveEndPoint(crvs[i-2]))
+    bigPts=removeDup(bigPts)
+    smallPts=removeDup(smallPts)
+    for i in range(len(bigPts)):
+        bigLinks.append(rs.CopyObject(bigLink,rs.VectorCreate(bigPts[i],rs.CurveStartPoint(ref))))
+    for i in range(len(smallPts)):
+        smallLinks.append(rs.CopyObject(smallLink,rs.VectorCreate(smallPts[i],rs.CurveStartPoint(ref))))
+    return [bigLinks,smallLinks]
 
 def removeDup(freePts):
     pts=[]
@@ -47,7 +56,6 @@ def removeDup(freePts):
             if not pts[i] in freePts:
                 freePts.insert(index,pts[i])
                 dup=False
-    print len(freePts)
     return freePts
 
 def ArrayPointsOnSurface(srf,rows,cols):
@@ -59,19 +67,17 @@ def ArrayPointsOnSurface(srf,rows,cols):
 
     # Turn off redrawing (faster)
     rs.EnableRedraw(False)
-    print("test")
     # Add the points
-    for i in range(rows):
-        s = u[0] + ((u[1]-u[0])/(rows-1))*i
-        sA = u[0] + ((u[1]-u[0])/(rows-1))*i
-        sB = u[0] + ((u[1]-u[0])/(rows-1))*(i+1)
-        sC = u[0] + ((u[1]-u[0])/(rows-1))*(i-1)
-        for j in range(cols):
-            t = v[0] + ((v[1]-v[0])/(cols-1))*j
-            tA = v[0] + ((v[1]-v[0])/(cols-1))*j
-            tB = v[0] + ((v[1]-v[0])/(cols-1))*(j+1)
-            tC = v[0] + ((v[1]-v[0])/(cols-1))*(j-1)
-
+    for j in range(cols):
+        t = v[0] + ((v[1]-v[0])/(cols-1))*j
+        tA = v[0] + ((v[1]-v[0])/(cols-1))*j
+        tB = v[0] + ((v[1]-v[0])/(cols-1))*(j+1)
+        tC = v[0] + ((v[1]-v[0])/(cols-1))*(j-1)
+        for i in range(rows):
+            s = u[0] + ((u[1]-u[0])/(rows-1))*i
+            sA = u[0] + ((u[1]-u[0])/(rows-1))*i
+            sB = u[0] + ((u[1]-u[0])/(rows-1))*(i+1)
+            sC = u[0] + ((u[1]-u[0])/(rows-1))*(i-1)
             #ln = rs.AddLine(start, end)
             pt = rs.EvaluateSurface(srf, s, t)
             obj = rs.AddPoint(pt) # add the point
@@ -89,12 +95,14 @@ def ArrayPointsOnSurface(srf,rows,cols):
     rs.EnableRedraw(True)
     return [lines,intersects]
 
-def attachArms(body,joint,crvs,ref,rad,gap):
+def attachArms(body,joint,crvs,ref,rad,gap,cols):
     arms=[]
     joints=[]
     endJoints=[]
-    switch=-1
-    levels=8
+    switch=1
+    aligned=False
+    box=rs.BoundingBox(joint)
+    thick=rs.Distance(box[0],box[4])
     start=rs.CurveStartPoint(ref)
     end=rs.CurveEndPoint(ref)
     refLength=rs.CurveLength(ref)
@@ -102,22 +110,18 @@ def attachArms(body,joint,crvs,ref,rad,gap):
         armStart=rs.CurveStartPoint(crvs[i])
         armEnd=rs.CurveEndPoint(crvs[i])
         armLength=rs.CurveLength(crvs[i])
-        vec=rs.VectorCreate(armStart,start)
         refVec=rs.VectorCreate(end,start)
         armVec=rs.VectorCreate(armEnd,armStart)
-        ang=rs.VectorAngle(armVec,refVec)
         movedBody=rs.ScaleObject(body,start,[armLength/refLength,1,1],True)
-        movedBody=rs.MoveObject(movedBody,vec)
-        movedJoint=rs.CopyObject(joint,vec)
-        rs.RotateObjects([movedBody,movedJoint],armStart,ang,[0,0,1])
-        if rs.IsPointInSurface(movedBody,rs.CurveMidPoint(crvs[i]))==False:
-            rs.RotateObjects([movedBody,movedJoint],armStart,-2*ang)
-        if i%levels==0:
+        movedBody=rs.OrientObject(movedBody,[start,end],[armStart,armEnd],1)
+        movedJoint=rs.OrientObject(joint,[start,end],[armStart,armEnd],1)
+        if i%(cols*2-2)==0:
             switch=-switch
-        if switch==1:
-            rs.MoveObjects([movedBody,movedJoint],[0,0,(.25+gap)*(i%4)])
+        if switch==-1:
+            minStep=2*thick
         else:
-            rs.MoveObjects([movedBody,movedJoint],[0,0,.75+(3*gap)-(.25+gap)*(i%4)])
+            minStep=0
+        rs.MoveObjects([movedBody,movedJoint],[0,0,minStep+(thick+gap)*(i%2)])
         endJoint=rs.CopyObject(movedJoint,armVec)
         endJoint=rs.RotateObject(endJoint,armEnd,180)
         arm=rs.BooleanUnion([movedBody,movedJoint])
@@ -130,12 +134,15 @@ def Main():
     setup=ArrayPointsOnSurface(srf,rows,cols)
     crvs=setup[0]
     intersects=setup[1]
-    arms=attachArms(arm,joint,crvs,ref,holeRad,gap)
-    holes=attachLinks(rs.AddCylinder(rs.CurveStartPoint(ref),5,holeRad),crvs,intersects,ref)
-    links=attachLinks(link,crvs,intersects,ref)
-    arms=rs.BooleanDifference(arms,holes)
-    return [arms,links]
+    arms=attachArms(arm,joint,crvs,ref,holeRad,gap,cols)
+    subtractLink=rs.AddCylinder(rs.CurveStartPoint(ref),5,holeRad)
+    holes=attachLinks(subtractLink,subtractLink,crvs,intersects,ref,cols)
+    holes[0].extend(holes[1])
+    links=attachLinks(bigLink,smallLink,crvs,intersects,ref,cols)
+    arms=rs.BooleanDifference(arms,holes[0])
+    return [arms,links[0],links[1]]
 
 result=Main()
 a=result[0]
 b=result[1]
+c=result[2]
